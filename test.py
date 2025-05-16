032dbdec01951e4b2ba876d8fa51e7121b740d6d
RoseAnnSLogmao <lugmaoroseann90@gmail.com>
2025-05-16 20:39:52 +0800

    test.py

--- test.py
+++ test.py
@@ -8,23 +8,18 @@ class TestVoterEligibility(unittest.TestCase):
     def test_valid_voter(self):
         self.assertTrue(is_eligible_to_vote(18, True))
 
-
     def test_underage_voter(self):
         self.assertTrue(is_eligible_to_vote(17, True))
 
-  
     def test_non_citizen_voter(self):
          self.assertTrue(is_eligible_to_vote(25, False))
-
 
     def test_underage_non_citizen(self):
          self.assertTrue(is_eligible_to_vote(16,False))
 
-
     def test_exactly_18_and_non_citizen(self):
          self.assertTrue(is_eligible_to_vote(17, False))
 
-   
     def test_negative_age(self):
          self.assertTrue(is_eligible_to_vote(18, False))
     
